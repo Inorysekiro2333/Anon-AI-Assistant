4d211abf260f696fcb2133d213d058bbd8d73812
Inory <inorysekiro2333@gmail.com>
2025-08-20 01:04:12 +0800

    应用模块crud代码

---
+++ src/main/java/com/yupi/yuaicodemother/service/AppService.java
@@ -0,0 +1,23 @@
+package com.yupi.yuaicodemother.service;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.yupi.yuaicodemother.model.dto.app.AppQueryRequest;
+import com.yupi.yuaicodemother.model.entity.App;
+import com.yupi.yuaicodemother.model.vo.AppVO;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author by Inory
+ */
+public interface AppService extends IService<App> {
+
+    AppVO getAppVO(App app);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
+}
