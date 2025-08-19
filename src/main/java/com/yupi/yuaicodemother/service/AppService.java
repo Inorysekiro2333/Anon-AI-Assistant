5685b017458647d592fef4d33f4bbca8a7dd185f
Inory <inorysekiro2333@gmail.com>
2025-08-20 01:13:08 +0800

    AppService添加注释

--- src/main/java/com/yupi/yuaicodemother/service/AppService.java
+++ src/main/java/com/yupi/yuaicodemother/service/AppService.java
@@ -15,9 +15,27 @@ import java.util.List;
  */
 public interface AppService extends IService<App> {
 
+    /**
+     * 获取应用封装类
+     *
+     * @param app
+     * @return
+     */
     AppVO getAppVO(App app);
 
+    /**
+     * 构造应用查询条件
+     *
+     * @param appQueryRequest
+     * @return
+     */
     QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 
+    /**
+     * 获取应用封装类列表
+     *
+     * @param appList
+     * @return
+     */
     List<AppVO> getAppVOList(List<App> appList);
 }
